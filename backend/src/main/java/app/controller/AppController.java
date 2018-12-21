5c6535cf41e9159b0b7351a9ad99cf8cbf6c4c1f
Magdalena Szkaradek <magda.szkaradek@student.uj.edu.pl>
2018-12-21 21:35:18 +0100

    Save graph

--- backend/src/main/java/app/controller/AppController.java
+++ backend/src/main/java/app/controller/AppController.java
@@ -3,22 +3,31 @@ package app.controller;
 import app.objects.Graph;
 import app.service.GraphService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.*;
 import java.util.*;
 
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
-@RequestMapping(path = "/")
+@RequestMapping(path = "/graph")
 public class AppController {
     @Autowired
     GraphService graphService;
 
-    @GetMapping("/graph")
+    @GetMapping
     public Map<Integer, ArrayList<Integer>> getGraph() {
         Graph graph = graphService.buildGraph("graphExample.txt");
         return graph.nodes;
+    }
+
+    @PostMapping
+    public ResponseEntity saveGraph(@RequestBody Graph graph) throws IOException {
+        graphService.saveGraph(graph);
+        return ResponseEntity.ok().build();
+
     }
 
 
--- backend/src/main/java/app/service/GraphService.java
+++ backend/src/main/java/app/service/GraphService.java
@@ -3,12 +3,13 @@ package app.service;
 import app.objects.Graph;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Scanner;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @Service
 public class GraphService {
@@ -49,5 +50,37 @@ public class GraphService {
             graph.nodes.put(nodeNumber, nodes);
         }
         return graph;
+    }
+
+    public void saveGraph(Graph graph) throws IOException {
+        String fileName = createFile();
+        List<String> lines = createContentToSave(graph);
+
+        Path file = Paths.get(fileName);
+        Files.write(file, lines, Charset.forName("UTF-8"));
+    }
+
+    private List<String> createContentToSave(Graph graph) {
+        List<String> lines = new ArrayList<>();
+
+        lines.add(String.valueOf(graph.numberOfNodes));
+        for (int i = 1; i < graph.nodes.size() + 1; i++) {
+            ArrayList<Integer> neighboursList = graph.nodes.get(i);
+            String neighboursAsString = "";
+            for (Integer neighbour : neighboursList) {
+                neighboursAsString += String.valueOf(neighbour) + " ";
+            }
+            String line = i + " " + graph.nodes.get(i).size() + " " + neighboursAsString;
+            lines.add(line);
+        }
+        return lines;
+    }
+
+    private String createFile() throws FileNotFoundException, UnsupportedEncodingException {
+        String fileName = LocalDateTime.now().toString() + ".txt";
+        fileName = fileName.replaceAll(":", "");
+
+        new PrintWriter(fileName, "UTF-8");
+        return fileName;
     }
 }
